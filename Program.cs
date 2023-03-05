3bc84a4e68870bed3d0fdd17639260a51b8dc0fa
natashandrigoz <nata_shandrigoz@mail.ru>
2023-03-05 20:34:15 +0200

    Генерируются 10 случайных целых чисел чисел в интервале (-40, 40). Вывести эти числа

--- Program.cs
+++ Program.cs
@@ -10,6 +10,13 @@ namespace ConsoleApp5
     {
         static void Main(string[] args)
         {
+            //1.Генерируются 10 случайных целых чисел чисел в интервале (-40, 40). Вывести эти числа
+            Random random = new Random();
+            for (int i=1;i<=10;i++)
+            {
+                int element = random.Next(-40, 40);
+                Console.WriteLine(element);
+            }
             //1.Генерируется 8 случайных чисел в интервале (–30, 30). Выводятся эти
             //числа и сообщения: отрицательное – положительное, четное – нечетное(for, if)
             //Random random = new Random();
@@ -39,23 +46,22 @@ namespace ConsoleApp5
             //}
             //2. Генерируется 10 случайных чисел в интервале (– 20, 20). Выводятся толь-
             //ко положительные числа и сообщения: четное – нечетное(for, if)
-            Random random1 = new Random();
-            for (int j=1;j<=10;j++)
-            {
-                int element1 = random1.Next(-20, 20);
-                if (element1>0)
-                    if (element1%2==0)
-                {
-                    Console.WriteLine($"{element1}\t-положительное, чётное");
-                }
-                else
-                {
-                    Console.WriteLine($"{element1}\t-положительное, нечётное");
-                }
-            }
-            
-            
-            
+            //Random random1 = new Random();
+            //for (int j=1;j<=10;j++)
+            //{
+            //    int element1 = random1.Next(-20, 20);
+            //    if (element1>0)
+            //        if (element1%2==0)
+            //    {
+            //        Console.WriteLine($"{element1}\t-положительное, чётное");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"{element1}\t-положительное, нечётное");
+            //    }
+            //}
+
+
             Console.Read();
         }
     }
