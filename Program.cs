610e76867683ffe86bd0e3aeaaa2c95588745dc3
natashandrigoz <nata_shandrigoz@mail.ru>
2023-03-05 19:57:47 +0200

    Генерируется 8 случайных чисел в интервале (–30, 30). Выводятся эти числа и сообщения: отрицательное – положительное, четное – нечетное (for, if)

--- Program.cs
+++ Program.cs
@@ -10,6 +10,34 @@ namespace ConsoleApp5
     {
         static void Main(string[] args)
         {
+            //1.Генерируется 8 случайных чисел в интервале (–30, 30). Выводятся эти
+            // числа и сообщения: отрицательное – положительное, четное – нечетное(for, if)
+            Random random = new Random();
+            for (int i=1;i<=8;i++)
+            {
+                int element = random.Next(-30, 30);
+                if (element>0)
+                {
+                    Console.WriteLine($"{element}\t-положительное");
+                }
+                else if (element<0)
+                {
+                    Console.WriteLine($"{element}\t-отрицательное");
+                }
+                else
+                {
+                    Console.WriteLine($"{element}\t=0");
+                }
+                if (element%2==0)
+                {
+                    Console.WriteLine($"{element}\t-чётное");
+                }
+                else
+                {
+                    Console.WriteLine($"{element}\t-нечётное");
+                }
+            }
+            Console.Read();
         }
     }
 }
