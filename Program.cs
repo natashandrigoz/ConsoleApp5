0909f9c2703eee3d2a0a87964c5b318695df0cb4
natashandrigoz <nata_shandrigoz@mail.ru>
2023-03-05 20:12:51 +0200

    Генерируется 10 случайных чисел в интервале (– 20, 20). Выводятся толь- ко положительные числа и сообщения: четное – нечетное (for, if)

--- Program.cs
+++ Program.cs
@@ -11,32 +11,51 @@ namespace ConsoleApp5
         static void Main(string[] args)
         {
             //1.Генерируется 8 случайных чисел в интервале (–30, 30). Выводятся эти
-            // числа и сообщения: отрицательное – положительное, четное – нечетное(for, if)
-            Random random = new Random();
-            for (int i=1;i<=8;i++)
+            //числа и сообщения: отрицательное – положительное, четное – нечетное(for, if)
+            //Random random = new Random();
+            //for (int i=1;i<=8;i++)
+            //{
+            //    int element = random.Next(-30, 30);
+            //    if (element>0)
+            //    {
+            //        Console.WriteLine($"{element}\t-положительное");
+            //    }
+            //    else if (element<0)
+            //    {
+            //        Console.WriteLine($"{element}\t-отрицательное");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"{element}\t=0");
+            //    }
+            //    if (element%2==0)
+            //    {
+            //        Console.WriteLine($"{element}\t-чётное");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"{element}\t-нечётное");
+            //    }
+            //}
+            //2. Генерируется 10 случайных чисел в интервале (– 20, 20). Выводятся толь-
+            //ко положительные числа и сообщения: четное – нечетное(for, if)
+            Random random1 = new Random();
+            for (int j=1;j<=10;j++)
             {
-                int element = random.Next(-30, 30);
-                if (element>0)
+                int element1 = random1.Next(-20, 20);
+                if (element1>0)
+                    if (element1%2==0)
                 {
-                    Console.WriteLine($"{element}\t-положительное");
-                }
-                else if (element<0)
-                {
-                    Console.WriteLine($"{element}\t-отрицательное");
+                    Console.WriteLine($"{element1}\t-положительное, чётное");
                 }
                 else
                 {
-                    Console.WriteLine($"{element}\t=0");
-                }
-                if (element%2==0)
-                {
-                    Console.WriteLine($"{element}\t-чётное");
-                }
-                else
-                {
-                    Console.WriteLine($"{element}\t-нечётное");
+                    Console.WriteLine($"{element1}\t-положительное, нечётное");
                 }
             }
+            
+            
+            
             Console.Read();
         }
     }
